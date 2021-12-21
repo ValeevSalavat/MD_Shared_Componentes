de961ac1f45ca290bcaab2a756dc621dcb2cf580
salavat.valeev <salik95@mail.ru>
2021-12-21 16:43:48 +0400

    some changes

--- shared/Button.tsx
+++ shared/Button.tsx
@@ -1,3 +1,5 @@
-const Button=()=>{
-    return <div styles='{{backgroundColor:red}}'>DefaultButton</div>
+import {React} from 'react'
+
+const Button = (text:string)=>{
+    return <div styles='{{backgroundColor:red}}'>{text}</div>
 }
